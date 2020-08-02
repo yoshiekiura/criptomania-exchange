46294b8c8e2fdd2b8eae48c706ca35c0a0b43b95
faturprayuda <faturprayuda@gmail.com>
2020-08-02 15:15:58 +0700

    add new font

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,87 +1,69 @@
 @include('frontend.mobiris._header')
+
 <body>
 
-<section class="menu cid-qynYwP0rAw" once="menu" id="menu1-3" data-rv-view="1014">
-@include('frontend.mobiris._top_navbar')
+  <section class="menu cid-qynYwP0rAw" once="menu" id="menu1-3" data-rv-view="1014">
+    @include('frontend.mobiris._top_navbar')
 
 
-</section>
+  </section>
 
-<section class="header4 cid-qyk0Y195zN" id="header4-1" data-rv-view="993">
-@include('frontend.mobiris._banner')
+  <section class="header4 cid-qyk0Y195zN" id="header4-1" data-rv-view="993">
+    @include('frontend.mobiris._banner')
 
-</section>
+  </section>
 
-<section class="mbr-section info2 cid-qypuIzB65Z" id="info2-h" data-rv-view="1002">
-     <div class="container">
+  <section class="mbr-section info2 cid-qypuIzB65Z" id="info2-h" data-rv-view="1002">
+    <div class="container" style="margin-top: -150px;">
 
-                    <div class="glide">
-              <div class="glide__track" data-glide-el="track">
-                
-                 <ul class="glide__slides">
-                @foreach($stockPairs as $stockPair)
+      <div class="glide">
+        <div class="glide__track" data-glide-el="track">
 
-                  <li class="glide__slide">
+          <ul class="glide__slides">
+            @foreach($stockPairs as $stockPair)
 
-                    <h5 style="font-size:15px">{{ $stockPair->stock_item_abbr }}/{{ $stockPair->base_item_abbr }}</h5>
-                    <h5 style="font-size:15px">Last Price: {{ number_format($stockPair->last_price) }}</h5>
-                    <h5 style="font-size:15px">
-                      
-                                        @if($stockPair->change_24 > 0)
-                                            <i class="fa fa-sort-up text-green"></i>
-                                        @elseif($stockPair->change_24 < 0)
-                                            <i class="fa fa-sort-down text-red"></i>
-                                        @else
-                                            <i class="fa fa-sort text-gray"></i>
-                                        @endif
-                    <span style="font-size:15px">{{ number_format($stockPair->change_24,2) }}%</span>
+            <li class="glide__slide">
 
-                                        
-                    </h5>
-                    <h5 style="font-size:15px">Volume: {{ number_format($stockPair->exchanged_base_item_volume_24, 3) }}</h5>
-                  </li>
-                  @endforeach
-                  
-                </ul>
+              <h5 style="font-size:15px">{{ $stockPair->stock_item_abbr }}/{{ $stockPair->base_item_abbr }}</h5>
+              <h5 style="font-size:15px">Last Price: {{ number_format($stockPair->last_price) }}</h5>
+              <h5 style="font-size:15px">
+
+                @if($stockPair->change_24 > 0)
+                <i class="fa fa-sort-up text-green"></i>
+                @elseif($stockPair->change_24 < 0) <i class="fa fa-sort-down text-red"></i>
+                  @else
+                  <i class="fa fa-sort text-gray"></i>
+                  @endif
+                  <span style="font-size:15px">{{ number_format($stockPair->change_24,2) }}%</span>
 
 
-              </div>
+              </h5>
+              <h5 style="font-size:15px">Volume: {{ number_format($stockPair->exchanged_base_item_volume_24, 3) }}</h5>
+            </li>
+            @endforeach
 
-          
+          </ul>
+
+
         </div>
+
+
+      </div>
     </div>
-</section>
+  </section>
 
-<section class="cid-qyjVcMTXLT mbr-parallax-background" id="header2-0" data-rv-view="996" style="background-image:url({{asset('frontend/images/parallax.jpg')}}); ">
-@include('frontend.mobiris._suggested')
-</section>
+  <section class="features1 cid-qyp9g9ptI2" id="features1-c" data-rv-view="999">
+    @include('frontend.mobiris._reason')
+  </section>
 
+  {{-- <section class="header3 cid-qyoZgHBpNG" id="header3-4" data-rv-view="1005"> --}}
+  @include('frontend.mobiris._about')
+  {{-- </section> --}}
 
-<section class="features1 cid-qyp9g9ptI2" id="features1-c" data-rv-view="999">
-@include('frontend.mobiris._reason')
-
-</section>
-
-<section class="mbr-section info2 cid-qypuIzB65Z" id="info2-h" data-rv-view="1002">
-@include('frontend.mobiris._major')
-</section>
-
-<section class="header3 cid-qyoZgHBpNG" id="header3-4" data-rv-view="1005">
-@include('frontend.mobiris._about')
-
-</section>
-
-<section class="cid-qyprfgE5MZ mbr-parallax-background" id="header3-8" data-rv-view="1008">
-@include('frontend.mobiris._data')
-</section>
+  @include('frontend.mobiris._footer')
 
 
-
-
-@include('frontend.mobiris._footer')
-
-
-  <script src="{{asset('mobiris/web/assets/jquery/jquery.min.js')}}"></script> 
+  <script src="{{asset('mobiris/web/assets/jquery/jquery.min.js')}}"></script>
   <script src="{{asset('mobiris/tether/tether.min.js')}}"></script>
   <script src="{{asset('mobiris/popper/popper.min.js')}}"></script>
   <script src="{{asset('mobiris/bootstrap/js/bootstrap.min.js')}}"></script>
@@ -94,11 +76,11 @@
   <script src="{{ asset('common/vendors/jquery-ui/jquery-ui.min.js') }}"></script>
   <script src="{{ asset('common/vendors/iCheck/icheck.min.js') }}"></script>
   <script src="{{ asset('backend/assets/js/custom.js') }}"></script>
-<script src="{{ asset('backend/assets/js/adminlte.min.js') }}"></script>
-<script src="{{ asset('backend/assets/js/custom.js') }}"></script>
+  <script src="{{ asset('backend/assets/js/adminlte.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/js/custom.js') }}"></script>
 
-<script>
-  var glide = new Glide('.glide', {
+  <script>
+    var glide = new Glide('.glide', {
   type: 'carousel',
   autoplay: 1500,
   perView: 4,
@@ -115,11 +97,12 @@
 })
 
 glide.mount()
-</script>
+  </script>
 
 
 
-  
-  
+
+
 </body>
+
 </html>
