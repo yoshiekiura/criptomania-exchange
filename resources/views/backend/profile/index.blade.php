b2c7727dc300ddbd22eeb5ec1631c5c939b49724
faturprayuda <faturprayuda@gmail.com>
2020-08-03 18:19:14 +0700

    edit avatar profile

--- resources/views/backend/profile/index.blade.php
+++ resources/views/backend/profile/index.blade.php
@@ -1,20 +1,20 @@
 @extends('backend.layouts.main_layout')
 @section('title', $title)
 @section('content')
-    <div class="row">
-        <div class="col-md-3">
-            <!-- Profile Image -->
-            @include('backend.profile.avatar', ['profileRouteInfo' => profileRoutes('user', $user->id)])
-        </div>
-        <div class="col-md-9">
-            <div class="nav-tabs-custom">
-                @include('backend.profile.profile_nav')
+<div class="row">
+    <div class="col-md-0">
+        <!-- Profile Image -->
+        {{-- @include('backend.profile.avatar', ['profileRouteInfo' => profileRoutes('user', $user->id)]) --}}
+    </div>
+    <div class="col-md-12">
+        <div class="nav-tabs-custom">
+            @include('backend.profile.profile_nav')
 
-                <div class="box box-solid">
-                    <div class="box-body">
-                        <div class="table-responsive">
-                            <table class="table">
-                                <tbody>
+            <div class="box box-solid">
+                <div class="box-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <tbody>
                                 <tr>
                                     <th>{{ __('Name') }}</th>
                                     <td>{{ $user->userInfo->full_name }}</td>
@@ -27,10 +27,12 @@
                                     <th>{{ __('Email') }}</th>
                                     <td>{{ $user->email }}
                                         @if( admin_settings('require_email_verification') == ACTIVE_STATUS_ACTIVE )
-                                            <span class="label label-{{ config('commonconfig.email_status.' . $user->is_email_verified . '.color_class') }}">{{ email_status($user->is_email_verified) }}</span>
-                                            @if($user->is_email_verified != EMAIL_VERIFICATION_STATUS_ACTIVE)
-                                                <a class="btn-link pull-right" href="{{ route('verification.form') }}">{{ __('Verify Account') }}</a>
-                                            @endif
+                                        <span
+                                            class="label label-{{ config('commonconfig.email_status.' . $user->is_email_verified . '.color_class') }}">{{ email_status($user->is_email_verified) }}</span>
+                                        @if($user->is_email_verified != EMAIL_VERIFICATION_STATUS_ACTIVE)
+                                        <a class="btn-link pull-right"
+                                            href="{{ route('verification.form') }}">{{ __('Verify Account') }}</a>
+                                        @endif
                                         @endif
                                     </td>
                                 </tr>
@@ -45,35 +47,38 @@
                                 <tr>
                                     <th>{{ __('Account Status') }}</th>
                                     <td>
-                                        <span class="label label-{{ config('commonconfig.account_status.' . $user->is_active . '.color_class') }}">{{ account_status($user->is_active) }}</span>
+                                        <span
+                                            class="label label-{{ config('commonconfig.account_status.' . $user->is_active . '.color_class') }}">{{ account_status($user->is_active) }}</span>
                                     </td>
                                 </tr>
                                 <tr>
                                     <th>{{ __('Financial Status') }}</th>
                                     <td>
-                                        <span class="label label-{{ config('commonconfig.financial_status.' . $user->is_financial_active . '.color_class') }}">{{ financial_status($user->is_financial_active) }}</span>
+                                        <span
+                                            class="label label-{{ config('commonconfig.financial_status.' . $user->is_financial_active . '.color_class') }}">{{ financial_status($user->is_financial_active) }}</span>
                                     </td>
                                 </tr>
                                 <tr>
                                     <th>{{ __('Maintenance Access Status') }}</th>
                                     <td>
-                                        <span class="label label-{{ config('commonconfig.maintenance_accessible_status.' . $user->is_accessible_under_maintenance . '.color_class') }}">{{ maintenance_accessible_status($user->is_accessible_under_maintenance) }}</span>
+                                        <span
+                                            class="label label-{{ config('commonconfig.maintenance_accessible_status.' . $user->is_accessible_under_maintenance . '.color_class') }}">{{ maintenance_accessible_status($user->is_accessible_under_maintenance) }}</span>
                                     </td>
                                 </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                            </tbody>
+                        </table>
                     </div>
-                    <div class="box-footer clearfix">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <a href="{{ route('profile.edit') }}"
-                                   class="btn btn-sm btn-info btn-flat btn-sm-block">{{ __('Edit Profile') }}</a>
-                            </div>
+                </div>
+                <div class="box-footer clearfix">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="{{ route('profile.edit') }}"
+                                class="btn btn-sm btn-info btn-flat btn-sm-block">{{ __('Edit Profile') }}</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
--- resources/views/backend/profile/profile_nav.blade.php
+++ resources/views/backend/profile/profile_nav.blade.php
@@ -1,22 +1,22 @@
 <ul class="nav nav-tabs">
     <li class="{{ is_current_route(['profile.index','profile.edit']) }}"><a
-                href="{{ route('profile.index') }}">{{ __('Profile') }}</a></li>
+            href="{{ route('profile.index') }}">{{ __('Profile') }}</a></li>
     <li class="{{ is_current_route('profile.change-password') }}"><a
-                href="{{ route('profile.change-password') }}">{{ __('Change Password') }}</a></li>
-    <li class="{{ is_current_route('profile.avatar.edit') }}"><a
-                href="{{ route('profile.avatar.edit') }}">{{ __('Change Avatar') }}</a></li>
+            href="{{ route('profile.change-password') }}">{{ __('Change Password') }}</a></li>
+    {{-- <li class="{{ is_current_route('profile.avatar.edit') }}"><a
+        href="{{ route('profile.avatar.edit') }}">{{ __('Change Avatar') }}</a></li> --}}
     <li class="{{ is_current_route('trader.upload-id.index') }}"><a
-                href="{{ route('trader.upload-id.index') }}">{{ __('Upload ID') }}</a></li>
+            href="{{ route('trader.upload-id.index') }}">{{ __('Upload ID') }}</a></li>
     <li class="{{ is_current_route('profile.google-2fa.create') }}"><a
-                href="{{ route('profile.google-2fa.create') }}">{{ __('Google Authentication') }}</a></li>
+            href="{{ route('profile.google-2fa.create') }}">{{ __('Google Authentication') }}</a></li>
     @if(admin_settings('referral'))
-        <li class="{{ is_current_route('profile.referral') }}"><a
-                    href="{{ route('profile.referral') }}">{{ __('Referral Link') }}</a></li>
+    <li class="{{ is_current_route('profile.referral') }}"><a
+            href="{{ route('profile.referral') }}">{{ __('Referral Link') }}</a></li>
     @endif
 
     @if(Auth::user()->user_role_management_id == 2 || Auth::user()->user_role_management_id == 3 )
     <li class="{{is_current_route('profile.create.bank')}}"><a
-                    href="{{route('profile.create.bank')}}">{{ __('Create New Bank') }}</a></li>
+            href="{{route('profile.create.bank')}}">{{ __('Create New Bank') }}</a></li>
     @endif
-        <!-- <li>{{__('Bank Number')}}</li> -->
+    <!-- <li>{{__('Bank Number')}}</li> -->
 </ul>
