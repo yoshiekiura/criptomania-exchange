33685782ce40a48c9aa2505cb55e305045dfcdfc
faturprayuda <faturprayuda@gmail.com>
2020-08-25 16:13:26 +0700

    edit paging log

--- resources/views/backend/layouts/includes/_header.blade.php
+++ resources/views/backend/layouts/includes/_header.blade.php
@@ -19,9 +19,11 @@
 
   <!-- for datatable and date picker -->
   <link rel="stylesheet" href="{{ asset('common/vendors/datepicker/datepicker.css') }}">
-  <link rel="stylesheet"
+  {{-- <link rel="stylesheet"
     href="{{ asset('common/vendors/datatable_responsive/datatables/plugins/bootstrap/datatables.bootstrap.css') }}">
-  <link rel="stylesheet" href="{{ asset('common/vendors/datatable_responsive/datatables/datatables.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('common/vendors/datatable_responsive/datatables/datatables.min.css') }}"> --}}
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.5/css/responsive.bootstrap4.min.css">
   <link rel="stylesheet" href="{{ asset('backend/assets/style.css') }}">
   <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
   {{-- new Template --}}
--- resources/views/vendor/laravel-log-viewer/log.blade.php
+++ resources/views/vendor/laravel-log-viewer/log.blade.php
@@ -110,9 +110,14 @@
 
 @section('script')
 <!-- for datatable and date picker -->
-<script src="{{asset('common/vendors/datatable_responsive/datatables/datatables.min.js')}}"></script>
+{{-- <script src="{{asset('common/vendors/datatable_responsive/datatables/datatables.min.js')}}"></script>
 <script src="{{asset('common/vendors/datatable_responsive/datatables/plugins/bootstrap/datatables.bootstrap.js')}}">
-</script>
+</script> --}}
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.5/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.5/js/responsive.bootstrap4.min.js"></script>
 <script>
     $(document).ready(function () {
             $('.dc-table').dataTable({
