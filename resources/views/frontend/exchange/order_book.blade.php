3581c43e02fde5c9a370137d56e54fbfb49e2a70
faturprayuda <faturprayuda@gmail.com>
2020-08-31 21:13:43 +0700

    edit interface buy & sell orders exchange

--- resources/views/frontend/exchange/order_book.blade.php
+++ resources/views/frontend/exchange/order_book.blade.php
@@ -1,115 +1,47 @@
 <div class="col-md-3">
-    <div class="order-book">
-        <h2 class="heading">Order Book</h2>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Price(BTC)</th>
-                    <th>Amount(ETH)</th>
-                    <th>Total(ETH)</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="red-bg-80">
-                    <td class="red">0.022572</td>
-                    <td>1.253415</td>
-                    <td>15.27648</td>
-                </tr>
-                <tr class="red-bg-60">
-                    <td class="red">0.020371</td>
-                    <td>1.205415</td>
-                    <td>15.25648</td>
-                </tr>
-                <tr class="red-bg-40">
-                    <td class="red">0.023572</td>
-                    <td>1.645415</td>
-                    <td>15.23648</td>
-                </tr>
-                <tr class="red-bg-20">
-                    <td class="red">0.032378</td>
-                    <td>1.206715</td>
-                    <td>15.25348</td>
-                </tr>
-                <tr class="red-bg-10">
-                    <td class="red">0.022573</td>
-                    <td>1.262415</td>
-                    <td>15.19648</td>
-                </tr>
-                <tr class="red-bg-8">
-                    <td class="red">0.154377</td>
-                    <td>1.225415</td>
-                    <td>15.35648</td>
-                </tr>
-                <tr class="red-bg-5">
-                    <td class="red">0.120373</td>
-                    <td>1.285415</td>
-                    <td>15.25648</td>
-                </tr>
-                <tr class="red-bg">
-                    <td class="red">0.028576</td>
-                    <td>1.291415</td>
-                    <td>15.26448</td>
-                </tr>
-            </tbody>
-            <tbody class="ob-heading">
-                <tr>
-                    <td>
-                        <span>Last Price</span>
-                        0.020367
-                    </td>
-                    <td>
-                        <span>USD</span>
-                        148.65
-                    </td>
-                    <td class="red">
-                        <span>Change</span>
-                        -0.51%
-                    </td>
-                </tr>
-            </tbody>
-            <tbody>
-                <tr class="green-bg">
-                    <td class="green">0.158373</td>
-                    <td>1.209515</td>
-                    <td>15.23248</td>
-                </tr>
-                <tr class="green-bg-5">
-                    <td class="green">0.020851</td>
-                    <td>1.206245</td>
-                    <td>15.25458</td>
-                </tr>
-                <tr class="green-bg-8">
-                    <td class="green">0.025375</td>
-                    <td>1.205715</td>
-                    <td>15.65648</td>
-                </tr>
-                <tr class="green-bg-10">
-                    <td class="green">0.020252</td>
-                    <td>1.205495</td>
-                    <td>15.24548</td>
-                </tr>
-                <tr class="green-bg-20">
-                    <td class="green">0.020373</td>
-                    <td>1.205415</td>
-                    <td>15.25648</td>
-                </tr>
-                <tr class="green-bg-40">
-                    <td class="green">0.020156</td>
-                    <td>1.207515</td>
-                    <td>15.28948</td>
-                </tr>
-                <tr class="green-bg-60">
-                    <td class="green">0.540375</td>
-                    <td>1.205915</td>
-                    <td>15.25748</td>
-                </tr>
-                <tr class="green-bg-80">
-                    <td class="green">0.020372</td>
-                    <td>1.205415</td>
-                    <td>15.25648</td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="active-order">
+        <ul class="nav nav-pills" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" data-toggle="pill" href="#buy_orders" role="tab"
+                    aria-selected="true">{{ __('BUY ORDERS') }}</a>
+            </li>
+            @auth
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="pill" href="#sell_orders" role="tab"
+                    aria-selected="false">{{ __('SELL ORDERS') }}</a>
+            </li>
+            @endauth
+        </ul>
+        <div class="tab-content">
+            <div class="tab-pane fade show active" id="buy_orders" role="tabpanel">
+                <table id="buy_order_table" class="table table-hover table-responsive small exchange-table"
+                    style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>{{ __('PRICE') }}</th>
+                            <th><span class="stock_item"></span></th>
+                            <th><span class="base_item"></span></th>
+                            <th class="hide_in_mobile_small">{{ __('SUM') }}(<span class="base_item"></span>)</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+            @auth
+            <div class="tab-pane fade show" id="sell_orders" role="tabpanel">
+                <table id="sell_order_table" class="table table-hover table-responsive small exchange-table"
+                    style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>{{ __('PRICE') }}</th>
+                            <th><span class="stock_item"></span></th>
+                            <th><span class="base_item"></span></th>
+                            <th class="hide_in_mobile_small">{{ __('SUM') }}(<span class="base_item"></span>)</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+            @endauth
+        </div>
     </div>
     <div class="market-history" style="overflow:auto">
         <ul class="nav nav-pills" role="tablist">
